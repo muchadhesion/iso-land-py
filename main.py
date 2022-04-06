b4ec877fc891d80ca1b6658fad4f9b1607a4eaa3
muchadhesion <github@morganism.info>
2022-04-06 23:39:57 +0100

    10x10 map with colours

--- main.py
+++ main.py
@@ -4,20 +4,17 @@ import pygame
 top = [ (1,2), (0,1), (1,0), (2,1) ]
 top_zoomed = [ (x*20,y*10) for (x,y) in top]
 
-origin = (100,100)
+origin = (190,100)
 
-BLOCK_COL = 0xffff00
+BLOCK_COL = 0xff4000
 
 def draw(screen):
     pygame.draw.rect(screen, 0x0000ff, (100,100,200,200))
-    
-    block(screen, 0,0, BLOCK_COL)
-    block(screen, 1,0, BLOCK_COL + 0x80)
-    block(screen, 2,0, BLOCK_COL + 0xf0)
-    block(screen, 0,1, BLOCK_COL - 0x004000)
-    block(screen, 1,1, BLOCK_COL - 0x004000)
-    block(screen, 2,1, BLOCK_COL - 0x004000)
 
+    for ix in range(10):
+        for iy in range(10):
+            coladd = ix * 0x10 + iy * 0x1000
+            block(screen, ix, iy, BLOCK_COL + coladd)
 
 def block(screen, x, y , col):
     ix = x * 20 - y * 20 + origin[0]
