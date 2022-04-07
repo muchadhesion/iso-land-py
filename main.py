a3b2358d7d3616579f2ec751f64924be48e30ca0
muchadhesion <github@morganism.info>
2022-04-07 21:27:30 +0100

    Block land

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import pygame
 import pygame as pg
 import math
 
+isox, isoy = 20, 10
+
 top = [ (1,2), (0,1), (1,0), (2,1) ]
-top_zoomed = [ (x*20,y*10) for (x,y) in top]
+top_zoomed = [ (x*isox,y*isoy) for (x,y) in top]
+front_right = [ (2,1), (2,10), (1, 11), (1,2)]
+front_right_zoomed = [ (x*isox,y*isoy) for (x,y) in front_right]
+front_left= [ (1,2), (1,11), (0,10), (0,1)]
+front_left_zoomed = [ (x*isox,y*isoy) for (x,y) in front_left]
 
 origin = (200,200)
 
@@ -15,9 +21,11 @@ BLOCK_COL = 0xff4000
 
 def draw(screen, t = 0):
     screen.blit( map_z, (100,100) )
-    
-    ox = int( 40 * math.sin(t/2000) + 50 )
-    oy = int( 40 * math.cos(t/1000) + 50 )
+    # t = 6000
+    ox = int( 38 * math.sin(t/2000) + 50 )
+    oy = int( 38 * math.cos(t/1000) + 50 )
+    # oy = 10
+    # ox = 80
 
     pygame.draw.rect(screen, 0xffffff, (ox*2 + 100, oy*2 + 100, 20,20))
     for ix in range(10):
@@ -25,14 +33,25 @@ def draw(screen, t = 0):
             x = ix + ox
             y = iy + oy
             h = map.get_at((x,y))[0]
+            hfr = map.get_at((x+1,y))[0]
+            hfl = map.get_at((x,y+1))[0]
+            d = 0
+            if h < hfr or h < hfl:
+                d = 30
+            if iy == 9 or ix == 9:
+                d = 30
             coladd = ((x * 0x10) & 0xff) + ((y * 0x1000) & 0xff00)
-            block(screen, ix, iy, col= BLOCK_COL + coladd, height=h)
+            block(screen, ix, iy, col= BLOCK_COL + coladd, height=h, depth=d)
 
-def block(screen, x, y , col=0xffff00, height = 0):
+def block(screen, x, y , col=0xffff00, height = 0, depth = 30):
     ix = x * 20 - y * 20 + origin[0]
     iy = x * 10 + y * 10 + origin[1] + height * 1
     t = [ (px+ix, py+iy) for (px,py) in top_zoomed]
     pygame.draw.polygon(screen, col, t )
-
+    if depth > 0:
+        fr = [ (px+ix, py+iy) for (px,py) in front_right_zoomed]
+        fl = [ (px+ix, py+iy) for (px,py) in front_left_zoomed]
+        pygame.draw.polygon(screen, 0x404040, fr )
+        pygame.draw.polygon(screen, 0x202020, fl )
 
 three.go(draw)
